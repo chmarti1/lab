d3d76902e992f306f7b60f9da1716864993c1ee1
Chris Martin <crm28@psu.edu>
2016-11-22 19:45:05 -0500

    lconfig.h L1870, changed incrementer from 2 to 1. STREAM_OUT# are UINT16 not 32.

--- test.c
+++ test.c
@@ -1,27 +1,21 @@
-#include "ldisplay.h"       // For the display helper functions
+#include "lconfig.h"
 #include <unistd.h>         // for usleep/sleep
 
-#define MAX_INPUT 128
-
 int main(void){
-    char charin = 0,go_f = 1;
-    char input[MAX_INPUT];
-    unsigned int count = 0;
-
-    clear_terminal();
-    setup_keypress();
-    print_param(8,30,"Execution count");
-    while(go_f){
-        count ++;
-        print_int(8,30,count);
-        LDISP_CGO(10,1);
-        fflush(stdout);
-        usleep(500000);
-        if(prompt_on_keypress(-1,"Enter a new execution count: ",input,MAX_INPUT)){
-            clear_terminal();
-            print_param(8,30,"Execution count");
-            sscanf(input,"%d",&count);
-        }
-    }
-    finish_keypress();
+    DEVCONF dconf[8];
+    unsigned int count;
+    int err;
+    double data[1024];
+    
+    err = load_config(dconf,8,"test.conf");
+    err = err || open_config(dconf,0);
+    err = err || upload_config(dconf,0);
+    show_config(dconf,0);
+    err = err || start_data_stream(dconf,0,64);
+    for(count=0; count<64; count+=1)
+        err = err || read_data_stream(dconf,0,data);
+    err = err || stop_data_stream(dconf,0);
+    close_config(dconf,0);
+    if(err) printf("ERROR!\n");
+    return err;
 }
